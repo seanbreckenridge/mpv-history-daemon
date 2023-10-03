306d578b7f2c64cfd15fb6c4b63e16a222b2e0e8
Sean Breckenridge <seanbrecke@gmail.com>
2023-10-03 10:21:10 -0700

    use setup.cfg file

--- setup.py
+++ setup.py
@@ -1,37 +1,4 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
+from setuptools import setup
 
-from pathlib import Path
-from setuptools import setup, find_packages
-
-requirements = Path("requirements.txt").read_text().strip().splitlines()
-
-long_description = Path("README.md").read_text()
-
-pkg = "mpv_history_daemon"
-setup(
-    author="Sean Breckenridge",
-    author_email="seanbrecke@gmail.com",
-    classifiers=[
-        "Development Status :: 3 - Alpha",
-        "Intended Audience :: End Users/Desktop",
-        "Natural Language :: English",
-        "Programming Language :: Python :: 3",
-    ],
-    description="Daemon which connects to active mpv instances, saving a history of what I watch/listen to",
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    install_requires=requirements,
-    python_requires=">=3.8",
-    extras_require={"optional": ["orjson"]},
-    name=pkg,
-    packages=find_packages(include=[pkg]),
-    package_data={pkg: ["py.typed"]},
-    entry_points={
-        "console_scripts": ["mpv-history-daemon = mpv_history_daemon.__main__:cli"]
-    },
-    license="http://www.apache.org/licenses/LICENSE-2.0",
-    scripts=["bin/mpv_history_daemon_restart"],
-    url="https://github.com/seanbreckenridge/mpv-history-daemon",
-    version="0.2.3",
-)
+if __name__ == "__main__":
+    setup()
